35bf35acb367435a3db9fbdb54e18063dae27531
Hugo Estrada <hugoestr@gmail.com>
2020-01-06 19:57:26 -0500

    Added space strucs

---
+++ lib/space.ex
@@ -0,0 +1,14 @@
+defmodule Oca.Space do
+  @moduledoc """
+  Documentation for Oca.Space
+  """
+
+  @doc """
+  Creates a new Space
+
+  ## Description
+  The space is at the heart of the Game of the Goose. It often has a name, and it can have a special event if you land on it.
+
+  """
+  defstruct name: "", player: "", event: {:description, :action}
+end
